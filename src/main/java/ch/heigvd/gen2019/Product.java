c2d6af749ef9d9898da173943512850be2e030ec
Lougranou <legrand.bruno@hotmail.com>
2019-05-03 11:45:48 +0200

    #6 move method getSizeFor in class Product to avoid Product to be a data class

--- src/main/java/ch/heigvd/gen2019/Product.java
+++ src/main/java/ch/heigvd/gen2019/Product.java
@@ -25,8 +25,27 @@ public class Product {
         return color;
     }
 
-    public int getSize() {
+    public int getSizeNumber() {
         return size;
+    }
+
+    public String getSizeString(){
+        switch (this.size) {
+            case 1:
+                return "XS";
+            case 2:
+                return "S";
+            case 3:
+                return "M";
+            case 4:
+                return "L";
+            case 5:
+                return "XL";
+            case 6:
+                return "XXL";
+            default:
+                return "Invalid Size";
+        }
     }
 
     public double getPrice() {
